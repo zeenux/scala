945313b850adf09aa629495bf1761783dbff292f
Zeeshan Hayat <zeenux@gmail.com>
2018-02-20 15:13:54 +0500

    Revert "Revert "Working""
    This reverts commit ccebc20312924841e4fbf20c9fba03225db3fa7f.

--- src/com/zee/amazondataset/AmazonDriver.java
+++ src/com/zee/amazondataset/AmazonDriver.java
@@ -4,11 +4,37 @@
  * and open the template in the editor.
  */
 package com.zee.amazondataset;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.util.*;
+import org.apache.hadoop.fs.*;
 
 /**
  *
  * @author zeenux
  */
-public class AmazonDriver {
+public class AmazonDriver extends Configured implements Tool {
+    
+    public static void main(String [] args) throws Exception{
+        
+        int result=ToolRunner.run(new Configuration(), new AmazonDriver(), args);
+    }
+    
+    public int run(String [] args) throws Exception{
+        Configuration cnf=getConf();
+        Job job=Job.getInstance(cnf,"Amazon DataSet");
+        job.setJarByClass(AmazonDriver.class);
+        job.setMapperClass(AmazonMapper.class);
+        //job.setMapOutputKeyClass(theClass);
+        job.setNumReduceTasks(0);
+        
+        FileInputFormat.addInputPath(job, new Path("/home/zeenux/DATASETS/amazon-meta-subset.txt"));
+        double rnd=Math.random();
+        FileOutputFormat.setOutputPath(job, new Path("/home/zeenux/op/op_"+rnd));
+        
+        return job.waitForCompletion(true)?0:1;
+    }
     
 }
--- src/com/zee/amazondataset/AmazonMapper.java
+++ src/com/zee/amazondataset/AmazonMapper.java
@@ -4,11 +4,29 @@
  * and open the template in the editor.
  */
 package com.zee.amazondataset;
-
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.io.*;
 /**
  *
  * @author zeenux
  */
-public class AmazonMapper {
+public class AmazonMapper extends Mapper<LongWritable,Text,Text,IntWritable> {
+    
+    @Override
+    public void map(LongWritable key, Text value, Context context){
+        System.out.println("*******");
+        System.out.println(value.toString());
+        System.out.println("*******");
+        String [] line=value.toString().split("\n");
+        
+        
+        for(String k:line){
+            if(k.contains("title"))
+            System.out.println(k);
+            else if(k.contains("rating"))
+                System.out.println(k);
+        }
+        //System.out.println("---KEY-----"+key+"--VALUE----"+value);
+    }
     
 }
